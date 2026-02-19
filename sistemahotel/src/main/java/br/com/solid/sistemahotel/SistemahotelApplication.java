be5d6f24dae1b67112f88a43384405b57e86fb2e
mell1601 <melissa_r_pereira>
2026-02-19 14:04:28 -0300

    chore: initial project structure

---
+++ sistemahotel/src/main/java/br/com/solid/sistemahotel/SistemahotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.solid.sistemahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemahotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemahotelApplication.class, args);
+	}
+
+}
---
+++ sistemahotel/src/test/java/br/com/solid/sistemahotel/SistemahotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solid.sistemahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemahotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
